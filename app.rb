3877c06ce01c9b67d25fc53f51c96a3142319f6b
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-11-27 01:27:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  
+  get '/' do 
+    @session = session 
+    erb :index   
+  end 
+  
+  post '/' do 
+    @item = params['item']
+    @session["item"] = @item
+   erb :checkout
+  end 
+  
 end
